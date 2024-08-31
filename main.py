08b67a85a1ba5bc87480b0594684b8e6b0e0cfe0
wy <wy@gmail.com>
2024-08-31 11:50:56 +0800

    完成 templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 # print(__name__)
@@ -10,15 +10,38 @@ app = Flask(__name__)
 def index():
     today = datetime.now()
     print(today)
-    return f"<h1>Hello Flask!<br>{today}</h1>"
+    name = "ddd"
+    # return f"<h1>Hello Flask!<br>{today}</h1>"
+    return render_template("index.html", date=today, nm=name)
 
 
-books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+# books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 
 @app.route("/books")
 def show_books():
-    return books
+    # print(books)
+    for key in books:
+        print(books[key])
+    # return books
+    return render_template("books.html", bks=books)
 
 
 @app.route("/book/<int:id>")
